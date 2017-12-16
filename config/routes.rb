b34926ce1deb0f642e578c6e4fe66f4d2640c85e
cristian vidal <cristian.vidal.lopez@gmail.com>
2017-12-16 12:06:18 -0300

    ADD: photo and name to User Registration

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,7 @@ Rails.application.routes.draw do
   resources :pending_tasks, only:[:update]
 
   devise_for :users, controllers: {
-        sessions: 'users/sessions'
+        registrations: 'users/registrations'
       }
-
-   
-
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171212231141) do
+ActiveRecord::Schema.define(version: 20171216144926) do
 
   create_table "pending_tasks", force: :cascade do |t|
     t.boolean "done", default: false
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 20171212231141) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
